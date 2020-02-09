b6e7da841ef6301c973c304e0d7baca9f715e7e5
picoriblade <wispwishbeltree@gmail.com>
2020-02-08 21:03:30 -0800

    commit 2 formatting and stdin option next

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # lab-0-comp-322
 making sense of ones and zeroes
+
+
+2/7/2020
+I'm gonna be honest. I didnt even look at the project till now so I dont have any commmits for 
+those past weeks. very sorry about that.
+Anyways, I'm trying to get gcc and whatever working on my computer.
+
+2/8/2020
+well i got the gcc working on here and made a hello world to test it
+
+got vim working and learned what i needed
+
+got makefiles working
+
+figured out how to run the exefile in the windows command line
+
+got it reading files and converting them to binary and ascii
+
+the parity checker works.  had an issue where the parity counter wasnt being reset on each loop
+fixed that
--- lab0.c
+++ lab0.c
@@ -7,9 +7,10 @@
 int main(int argc, char* argv[]){
 	int i=0, j=0;
 	int binToDec=0;
+	int parityCounter=0;
 
 	if(argc < 2){
-		argv[1] = "binfile.txt";
+		argv[1] = "binfile.txt"; //i dont want to type binfile.txt every time
 	}else{
 		//strcat(argv[1], ".txt");
 	}
@@ -29,17 +30,28 @@ int main(int argc, char* argv[]){
 			sz = read(fd, buffer, 1);
 			buffer[sz] = '\0';
 
+			if(buffer[0] == '\0'){
+				buffer[0] = '0';
+			}
+
 			if(j!=0){
 				binToDec = pow((atoi(buffer)*2), j) + binToDec;
 			}else{
 				binToDec = binToDec + atoi(buffer);
 			}
+			parityCounter = parityCounter + atoi(buffer);
 
 			printf("%s", buffer);
 		}
 		read(fd, buffer, 1);
-		printf(" %d, %c\n", binToDec, (char) binToDec);
+		printf(" %d, %c, %d, ", binToDec, (char) binToDec, parityCounter);
+		if(parityCounter%2 == 1){
+			printf("Odd\n");
+		}else{
+			printf("Even\n");
+		}
 		binToDec = 0;
+		parityCounter = 0;
 	}
 
 	return 0;
